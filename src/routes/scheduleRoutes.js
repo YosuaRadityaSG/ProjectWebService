11825158b879043c5a8fa36c35fbd6742320c856
SuryaHtitikoma <hia.surya@gmail.com>
2026-07-08 22:52:22 +0700

    update route, nambah auth

--- src/routes/scheduleRoutes.js
+++ src/routes/scheduleRoutes.js
@@ -5,11 +5,12 @@ const {
   updateSchedule,
   deleteSchedule,
 } = require("../controllers/scheduleController");
+const { authenticate, authorize } = require("../middlewares/auth");
 const router = express.Router();
 
-router.post("/", createSchedule);
+router.post("/", authenticate, authorize("admin"), createSchedule);
 router.get("/", getSchedules);
-router.put(":id", updateSchedule);
-router.delete(":id", deleteSchedule);
+router.put(":id", authenticate, authorize("admin"), updateSchedule);
+router.delete(":id", authenticate, authorize("admin"), deleteSchedule);
 
 module.exports = router;
